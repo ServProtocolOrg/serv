4545e240112096d21162ffe056da7b5aaa30b4b2
VictorTrustyDev <me@victortrusty.dev>
2024-01-14 15:31:35 +0700

    update comment

--- types/indexer.go
+++ types/indexer.go
@@ -8,14 +8,19 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// EVMTxIndexer defines the interface of custom eth tx indexer.
+// EVMTxIndexer defines the interface of the custom ETH-Tx indexer.
 type EVMTxIndexer interface {
-	// LastIndexedBlock returns -1 if indexer db is empty
+	// LastIndexedBlock returns the last block number which was indexed and flushed into database.
+	// Returns -1 if db is empty.
 	LastIndexedBlock() (int64, error)
+
+	// IndexBlock indexes all ETH Txs of the block.
+	// Notes: no guarantee data is flushed into database after this function returns, it might be flushed at later point.
 	IndexBlock(*tmtypes.Block, []*abci.ResponseDeliverTx) error
 
 	// GetByTxHash returns nil if tx not found.
 	GetByTxHash(common.Hash) (*TxResult, error)
+
 	// GetByBlockAndIndex returns nil if tx not found.
 	GetByBlockAndIndex(int64, int32) (*TxResult, error)
 }
